8f1cc7318d07e373d43970d3fc49392634add536
reemcsp <reem092006@gmail.com>
2020-11-20 18:49:26 +0200

    Updates

---
+++ precache-manifest.265958e424085bf0b2f7733a91cec387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a751bec2e665dbdd1a0d7e16da689464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3235e525a5f47e6c988d",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "2d94fac3f39b1a44d05b",
+    "url": "/static/css/main.5db5b2d9.chunk.css"
+  },
+  {
+    "revision": "3235e525a5f47e6c988d",
+    "url": "/static/js/2.6ea835ca.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.6ea835ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d94fac3f39b1a44d05b",
+    "url": "/static/js/main.4f27ae13.chunk.js"
+  },
+  {
+    "revision": "02dde6462064193d360e",
+    "url": "/static/js/runtime-main.fec1f3c3.js"
+  }
+]);
